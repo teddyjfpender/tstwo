2676b154ad4e547a37698ff678cbc351586a6765
Teddy Pender <92999717+teddyjfpender@users.noreply.github.com>
2025-05-18 13:05:04 +0100

    Update domainLineTwiddles.test.ts

--- packages/core/test/poly/domainLineTwiddles.test.ts
+++ packages/core/test/poly/domainLineTwiddles.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "bun:test";
+import { describe, it, expect } from "vitest";
 import { domainLineTwiddlesFromTree } from "../../src/poly/utils";
 
 class FakeCoset {
